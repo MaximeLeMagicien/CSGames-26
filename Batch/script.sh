a99dc652a370b1fca4c75d3425bc7fb18cd45966
Maxime CORDELLA <maxime.cordella.1@ulaval.ca>
2025-10-14 19:46:14 -0400

    push 2 fix condition

--- Batch/script.sh
+++ Batch/script.sh
@@ -1,6 +1,6 @@
 while IFS=, read -r customer_id name email age city subscription_type; do 
     if [[ -n "$customer_id" &&  -n "$name" &&  -n "$email" &&  -n "$age" && -n "$city" && -n "$subscription_type" ]]; then
-        if [[ $age -gt 0 || $email =~ .*@.*\..* ]]; then
+        if [[ $age -gt 0 && $email =~ .*@.*\..* ]]; then
             echo $customer_id,$name,$email,$age,$city,$subscription_type;
         fi
     fi
